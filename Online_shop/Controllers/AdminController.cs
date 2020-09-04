281d307da576893150a143d4e0fad3041e35a5ef
kazi rahi <kazirahi1234@gmail.com>
2020-09-04 21:05:47 +0600

    AddCatagoryButton

--- Online_shop/Controllers/AdminController.cs
+++ Online_shop/Controllers/AdminController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Online_shop.DAL;
+using Online_shop.Models;
+using Online_shop.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +13,40 @@ namespace Online_shop.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
         public ActionResult Dashboard()
         {
             return View();
         }
+
+
+        public ActionResult Catgories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+
+
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetail cd;
+            if (categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetail>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetail();
+            }
+            return View("UpdateCategory", cd);
+
+        }
+
     }
 }
